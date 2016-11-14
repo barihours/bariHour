02178abb2f83a03c030099362614ac7c45b217d9
jorgesosi <jsosinowicz@gmail.com>
2016-11-14 08:36:03 -0300

    se accede a firebase para cargar los datos de los bares y cervecerias, se cambia controller se crea un factory para leer los datos, se modifica el acceso a los datos en los controladores de listas y detalles

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -39,11 +39,60 @@ angular.module('starter.controllers', [])
             }
         };
     })
-.controller('AppCtrl', function($scope,MyService,$http) {
+//fabrica de array firebase
+.factory('Marks', function(){
+  var database = firebase.database();//se llama a a base de datos en firebase
+  var databaseRef= database.ref();
+  var marcadoresRef = databaseRef.child('/marcadores/');//se carga la regerencia a marcadores
+  var marcadores = [{}];//varable tipo array para cargar los marcdores
+  //referencia es una variable temporal para cargar el array
+  var referencias = marcadoresRef.on('value', function(data) {
+    var i = 0;
+    //console.log(data.val());
+    data.forEach(function(marcador) {
+      i++;
+      var arr2 = {};
+      //console.log('El marcador '+marcador.key +'es ' + marcador.val());
+      arr2 = {
+        key: marcador.key,
+        id:marcador.val().id,
+        descrip: marcador.val().descrip,
+        tipo: marcador.val().tipo,
+        start: marcador.val().start ,
+        end: marcador.val().end,
+        icon:marcador.val().icon,
+        lat: marcador.val().lat,
+        lng: marcador.val().lng,
+        image: marcador.val().image
+
+       };
+      marcadores.push(arr2);
+   });
+  });
+
+  return {
+    all: function() {//muestra todos los marcaodres
+      return marcadores;
+    },
+    /*remove: function(marcador) {//esta funcion no la utiizamos
+      chats.splice(chats.indexOf(marcador), 1);
+    },*/
+    get: function(marcadorId) {//crea los marcadores para pasarlos al controlador que los llama
+      for (var i = 0; i < marcadores.length; i++) {
+        if (marcadores[i].key === parseInt(marcadorId)) {
+          return marcadores[i];
+        }
+      }
+      return null;
+    }
+  };
+
+})
+.controller('AppCtrl', function($scope,MyService,Marks,$http) {
 
   $scope.myFunc = function(e) {
         MyService.setProperty(e);
-        //console.log(MyService.getProperty());
+        console.log("Get property",MyService.getProperty());
     };
   $scope.ubicacion = function(e) {
         MyService.setUbicacion(e);
@@ -52,35 +101,33 @@ angular.module('starter.controllers', [])
         MyService.setTipo(e);
       };
   
-    if(MyService.setArray()==null){
+    /*if(MyService.setArray()==null){
     $http.get('js/markers.json').then(function(response){
     MyService.setArray(response.data);
-            })}
+            })}*/
   })
   
-.controller('MapCtrl', function($scope,$cordovaGeolocation, $http, $filter, MyService) {
-$scope.centrado=(MyService.getUbicacion());
-$scope.opcion=(MyService.getProperty());
+.controller('MapCtrl', function($scope,$cordovaGeolocation, $http, $filter, MyService, Marks) {
+  $scope.centrado=(MyService.getUbicacion());
+  $scope.opcion=(MyService.getProperty());
   var options = {timeout: 10000, enableHighAccuracy: true};
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
- //defino estilo de mapa para borrar negocios
-    var myStyle = [
-    {
-    stylers: [
-      { hue: "#00ffe6" },
-      { saturation: -20 }
-    ]
-  },
+    //defino estilo de mapa para borrar negocios
+    var myStyle = [{
+      stylers: [{ 
+        hue: "#00ffe6" },
+        { 
+        saturation: -20 }
+      ]},
      
-{         
-          featureType: "poi.business", 
-          elementType: "labels", 
-          stylers: [ 
-              { visibility: "off" } 
-          ]
-        }
-        ];
-        var styledMap = new google.maps.StyledMapType(myStyle, {name: "Styled Map"});
+    {         
+      featureType: "poi.business", 
+      elementType: "labels", 
+      stylers: [{
+         visibility: "off" 
+       }]
+    }];
+    var styledMap = new google.maps.StyledMapType(myStyle, {name: "Styled Map"});
     //GPS aveces incorrecto
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
     latLngCentro = new google.maps.LatLng(-41.135893,  -71.310535);
@@ -106,46 +153,52 @@ $scope.opcion=(MyService.getProperty());
     var marker = new google.maps.Marker({
       map: $scope.map,
       position: latLng
-  });  
+    });  
     google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-
-    //Espera hasta que el mapa haya cargado
-    var today = $filter('date')(new Date(),'HH:mm:ss');
-//Marcadores
-    $scope.data=MyService.getArray();
-  //Loop json
-  angular.forEach($scope.data.marcadores, function(value, key){
-  //Verifica si esta entre el horario y cambia el icono
-  if ($scope.opcion==value.tipo|| $scope.opcion==0){
-  if (today>value.start && today<value.end){
-      //value.icon="";
-       var animation=google.maps.Animation.BOUNCE}
-  //Setea marcador
-  marker = new google.maps.Marker({
-      map: $scope.map,
-      animation: animation,
-      title: value.descrip,
-      icon: {url:value.icon},
-      position: {lat: value.lat,
-                  lng: value.lng}
-  });  
-
-    //Descripcion cada marcador   
-   var infoWindow = new google.maps.InfoWindow({
-      content: value.descrip
-    });
-   //Agrega Evento de click
-   google.maps.event.addListener(marker, 'click', function () {
-      infoWindow.open($scope.map, marker);
-  });
-       markers.push(marker);
-
-}
+      //Espera hasta que el mapa haya cargado
+      var today = $filter('date')(new Date(),'HH:mm:ss');
+      //Marcadores
+     //$scope.data=MyService.getArray();//desde json
+      $scope.data=Marks.all();// desde firebase
+      //Loop json
+      //angular.forEach($scope.data.marcadores, function(value, key){//desde json
+      console.log('estoy en el for each MapCtrl');
+      console.log('por cada value',$scope.opcion);
+      angular.forEach($scope.data, function(value, key){
+        console.log('por cada value',$scope.opcion);
+        //Verifica si esta entre el horario y cambia el icono
+        if ($scope.opcion==value.tipo|| $scope.opcion==0){
+          if (today>value.start && today<value.end){
+            //value.icon="";
+            var animation=google.maps.Animation.BOUNCE
+          }
+          //Setea marcador
+          marker = new google.maps.Marker({
+            map: $scope.map,
+            animation: animation,
+            title: value.descrip,
+            icon: {url:value.icon},
+            position: {
+              lat: value.lat,
+              lng: value.lng
+            }
+          });  
+          //Descripcion cada marcador   
+          var infoWindow = new google.maps.InfoWindow({
+            content: value.descrip
+          });
+          //Agrega Evento de click
+          google.maps.event.addListener(marker, 'click', function () {
+          infoWindow.open($scope.map, marker);
+          });
+          markers.push(marker);
+        }
+      })
+    })
+  })
 })
-})
-})
-})
-.controller('Lista',function($scope, MyService) {
+//controladores de las listas de ceervecerias y bares desde json
+/*.controller('Lista',function($scope, MyService) {
 $scope.data=MyService.getArray();
 $scope.tipo=MyService.getTipo();
 $scope.detalle = function(e) {
@@ -153,16 +206,33 @@ $scope.detalle = function(e) {
     };
 
 
+})*/
+//controladores de las listas de cerveceria y bares desde firebase
+.controller('Lista',function($scope, Marks, MyService) {
+//$scope.data=MyService.getArray();
+$scope.data=Marks.all();
+$scope.tipo=MyService.getTipo();
+$scope.detalle = function(e) {
+        MyService.setItem(e);
+    };
+
+
 })
-.controller('Detalles',function($scope, MyService) {
+//controladores de la pagina de detalles de las cervecerias y bares desd json
+/*.controller('Detalles',function($scope, MyService) {
 $scope.data=MyService.getArray();
 $scope.id=MyService.getItem();
 console.log($scope.id);
 $scope.item=$scope.data.marcadores[$scope.id];
 console.log($scope.item);
-
-
-
+});*/
+//controladores de la pagina de detalles de las cervecerias y bares desd json
+.controller('Detalles',function($scope, Marks, MyService) {
+$scope.data=Marks.all();
+$scope.id=MyService.getItem();
+console.log("myservice scope id",$scope.id);
+$scope.item=$scope.data[$scope.id +1];// busca el item en array de firebase, suma uno para traer ee correcto
+console.log("detalles scope item",$scope.item);
 });
 /*directionsDisplay = new google.maps.DirectionsRenderer({
               });
